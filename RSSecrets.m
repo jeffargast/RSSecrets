292857e2fa3ed60dfebd0b7956858cfb4c59bcb5
Jeff Argast <jeffargast@mac.com>
2012-05-25 17:45:38 -0400

    Updated license

--- RSSecrets.m
+++ RSSecrets.m
@@ -2,8 +2,25 @@
 //  RSSecrets.m
 //  Secrets
 //
-//  Created by Jeff Argast on 4/13/12.
-//  Copyright (c) 2012 Jeff Argast. All rights reserved.
+//
+// Copyright (C) 2012, Jeff Argast. All rights reserved.
+// 
+// The author make NO WARRANTY or representation, either express or implied,
+// with respect to this software, its quality, accuracy, merchantability, or
+// fitness for a particular purpose.  This software is provided "AS IS", and you,
+// its user, assume the entire risk as to its quality and accuracy.
+// 
+// Permission is hereby granted to use, copy, modify, and distribute this
+// software or portions thereof for any purpose, without fee, subject to these
+// conditions:
+// 
+// (1) If any part of the source code is distributed, then this
+// statement must be included, with this copyright and no-warranty notice
+// unaltered.
+// 
+// (2) Permission for use of this software is granted only if the user accepts
+// full responsibility for any undesirable consequences; the author accepts
+// NO LIABILITY for damages of any kind.
 //
 
 #import "RSSecrets.h"
